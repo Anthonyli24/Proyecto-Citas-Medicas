d21fd297af22c5ba0664ca4675cdb1c3b753a71f
Anthony Li Perera <anthonyliperera08@gmail.com>
2025-03-15 16:31:54 -0600

    Add files via upload

---
+++ src/main/java/com/example/proyecto_sistema_citas/ProyectoSistemaCitasApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyecto_sistema_citas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoSistemaCitasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoSistemaCitasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/proyecto_sistema_citas/ProyectoSistemaCitasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyecto_sistema_citas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoSistemaCitasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
