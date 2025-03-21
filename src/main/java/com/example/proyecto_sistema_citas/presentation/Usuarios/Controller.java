1c0dab8218f2b66265051051d5113fc11d3ea938
Sebastian Alvarez Gomez <sebastian.alvarez.gomez@est.una.ac.cr>
2025-03-21 14:55:46 -0600

    John login

--- src/main/java/com/example/proyecto_sistema_citas/presentation/Usuarios/Controller.java
+++ src/main/java/com/example/proyecto_sistema_citas/presentation/Usuarios/Controller.java
@@ -1,8 +1,10 @@
 package com.example.proyecto_sistema_citas.presentation.Usuarios;
 
 import com.example.proyecto_sistema_citas.logic.Service;
+import com.example.proyecto_sistema_citas.logic.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -12,7 +14,8 @@ public class Controller {
     private Service service;
 
     @GetMapping("/home")
-    public String ListarUsuarios(Model model) {
+    public String ListarUsuarios(Model model, @AuthenticationPrincipal(expression = "Usuario") Usuario usuario) {
+        System.out.println(usuario);
         model.addAttribute("usuarios", service.usuarioFindAll());
         return "/presentation/Home/home";
     }
